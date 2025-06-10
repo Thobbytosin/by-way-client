a7830f45c9f915f9dad72926fc651e004c998f10
Falode Tobi <thobbytosin11@gmail.com>
2025-06-10 13:15:46 +0100

    remove middleware

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,50 +1,13 @@
-"use client";
+import ProfilePage from "@/components/Profile/ProfilePage";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-import React, { useEffect, useState } from "react";
-import Heading from "@/utils/Heading";
-import Header from "@/components/Header";
-import { useSelector } from "react-redux";
-import Profile from "@/components/Profile/Profile";
-import Loader from "@/components/Loader/Loader";
-import { RootState } from "@/redux/store";
-import { useServerStatus } from "@/hooks/api/useServerStatus";
-import ServerErrorUI from "@/components/Home/ServerErrorUI";
+export default function DashboardPage() {
+  const token = cookies().get("access_Token")?.value;
 
-type Props = {};
-
-const Page = (props: Props) => {
-  const { user } = useSelector((state: RootState) => state.auth);
-  const [isMounted, setIsMounted] = useState(false); // Track if component is mounted
-  const { error: serverError, isLoading: serverLoading } = useServerStatus({
-    checkInterval: 10000,
-  });
-
-  useEffect(() => {
-    setIsMounted(true); // Set to true once the component is mounted
-  }, []);
-
-  if (!isMounted) {
-    return <Loader key={"loading"} />;
+  if (!token) {
+    redirect("/");
   }
 
-  return (
-    <>
-      <Heading
-        title={`${user?.name} Profile - ByWay Learning Management System`}
-        description="This is an online e-learning platform where people can have access to resources for learning"
-        keywords="Programming, MERN, TypeScript, ReactJs, NextJs, Web development"
-      />
-
-      <Header />
-      {serverLoading ? (
-        <Loader key={"loading"} />
-      ) : serverError ? (
-        <ServerErrorUI errorMessage={serverError} />
-      ) : (
-        <Profile />
-      )}
-    </>
-  );
-};
-
-export default Page;
+  return <ProfilePage />;
+}
---
+++ components/Profile/ProfilePage.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Heading from "@/utils/Heading";
+import Header from "@/components/Header";
+import { useSelector } from "react-redux";
+import Profile from "@/components/Profile/Profile";
+import Loader from "@/components/Loader/Loader";
+import { RootState } from "@/redux/store";
+import { useServerStatus } from "@/hooks/api/useServerStatus";
+import ServerErrorUI from "@/components/Home/ServerErrorUI";
+
+type Props = {};
+
+const ProfilePage = (props: Props) => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [isMounted, setIsMounted] = useState(false); // Track if component is mounted
+  const { error: serverError, isLoading: serverLoading } = useServerStatus({
+    checkInterval: 10000,
+  });
+
+  useEffect(() => {
+    setIsMounted(true); // Set to true once the component is mounted
+  }, []);
+
+  if (!isMounted) {
+    return <Loader key={"loading"} />;
+  }
+
+  return (
+    <>
+      <Heading
+        title={`${user?.name} Profile - ByWay Learning Management System`}
+        description="This is an online e-learning platform where people can have access to resources for learning"
+        keywords="Programming, MERN, TypeScript, ReactJs, NextJs, Web development"
+      />
+
+      <Header />
+      {serverLoading ? (
+        <Loader key={"loading"} />
+      ) : serverError ? (
+        <ServerErrorUI errorMessage={serverError} />
+      ) : (
+        <Profile />
+      )}
+    </>
+  );
+};
+
+export default ProfilePage;
