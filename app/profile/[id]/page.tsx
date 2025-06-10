b3fea3511b43c0c961a7f9c9d83eae826ba3a67b
Falode Tobi <thobbytosin11@gmail.com>
2025-06-10 14:19:42 +0100

    add middleaare

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -3,12 +3,12 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function DashboardPage() {
-  const token = cookies().get("access_token")?.value;
-  console.log("TOKEN:", token);
+  // const token = cookies().get("access_token")?.value;
+  // console.log("TOKEN:", token);
 
-  if (!token) {
-    redirect("/");
-  }
+  // if (!token) {
+  //   redirect("/");
+  // }
 
   return <ProfilePage />;
 }
---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const protectedPaths = ["/profile", "/admin", "/course-class"];
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  // run check only on the selected routes
+  const isProtected = protectedPaths.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (!isProtected) {
+    return NextResponse.next(); // continue if the route is not part of the protected routes
+  }
+
+  //   get access token
+  const access_token = request.cookies.get("access_token")?.value;
+
+  console.log("ACCESS TOKEN:", access_token);
+
+  if (!access_token) {
+    // redirect to home page if there is no token
+    const homeUrl = new URL("/", request.url);
+
+    return NextResponse.redirect(homeUrl);
+  }
+
+  // if there is token, user can proceed
+  return NextResponse.next();
+}
